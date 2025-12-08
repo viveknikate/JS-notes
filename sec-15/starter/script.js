fab8f5dadc173b150e13985072c4d2b3286bd582
Vivek <viveknikate.2000@gmail.com>
2025-12-08 21:41:15 +0530

    Async-await

--- sec-15/starter/script.js
+++ sec-15/starter/script.js
@@ -10,3 +10,20 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(function (position) {
+     const { latitude, longitude } = position.coords;
+
+     const map = L.map('map').setView([latitude, longitude], 13);
+
+     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     }).addTo(map);
+
+     L.marker([51.5, -0.09]).addTo(map)
+          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .openPopup();
+
+}, function () {
+     alert('Could not get your position')
+})
--- sec-16/starter/script.js
+++ sec-16/starter/script.js
@@ -4,9 +4,9 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
-// https://restcountries.com/v2/name/portugal
+// https://restcountries.com/v2/name/india
 
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
-///////////////////////////////////////
+///////////////////////////////////
